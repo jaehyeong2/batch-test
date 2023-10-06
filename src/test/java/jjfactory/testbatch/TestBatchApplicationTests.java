cd9704750be5ba4f07ecf4bc1f8cee1109c7fd16
jaehyeong2 <wogud1514@gmail.com>
2023-10-06 15:19:39 +0900

    init

---
+++ src/test/java/jjfactory/testbatch/TestBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package jjfactory.testbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
